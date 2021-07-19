b1a15d451a094b4fe445c921a264d2440656dfaf
Dang <huy.dang@bbv,vn>
2021-07-19 20:10:07 +0700

    add

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ const knex = require('knex')({
     host: process.env.DB_HOST || '127.0.0.1',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PWD || 'root',
-    database: process.env.DB_NAME || 'qlbh',
+    database: process.env.DB_NAME || 'QLBH-MySQL-v3',
     port: process.env.DB_PORT || 8889
   },
   pool: {
